6fa5458b459a314a78b589895352bac08b95eeb4
Panav Munshi <pmunshi2@illinois.edu>
2020-12-08 16:08:25 -0800

    ObjectDetector.py: Refactor variable names to match PEP standard.

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -2,10 +2,10 @@ import cv2
 
 threshold_value = 0.45  # Threshold to detect object
 
-classNames = []
+class_names = []
 classFile = 'coco.names'
 with open(classFile, 'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
+    class_names = f.read().rstrip('\n').split('\n')
 
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 weightsPath = 'frozen_inference_graph.pb'
@@ -18,28 +18,26 @@ net.setInputSwapRB(True)
 
 
 def read_img(img, draw=True, object_list=[], json_config=None):
-    classIds, confs, bbox = net.detect(img, confThreshold=threshold_value, nmsThreshold=0.)
-    # print(classIds,bbox)
-
+    class_ids, confidence_values, bbox = net.detect(img, confThreshold=threshold_value, nmsThreshold=0.)
     if len(object_list) == 0:
-        object_list = classNames
+        object_list = class_names
 
-    objectInfo = []
-    if len(classIds) != 0:
-        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-            className = classNames[classId - 1]
-            if className in object_list:
-                objectInfo.append([box, className])
+    object_info = []
+    if len(class_ids) != 0:
+        for class_id, confidence_value, box in zip(class_ids.flatten(), confidence_values.flatten(), bbox):
+            class_name = class_names[class_id - 1]
+            if class_name in object_list:
+                object_info.append([box, class_name])
                 if draw:
                     cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-                    cv2.putText(img, className.upper(), (box[0] + 10, box[1] + 30),
+                    cv2.putText(img, class_name.upper(), (box[0] + 10, box[1] + 30),
                                 cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-                    cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
+                    cv2.putText(img, str(round(confidence_value * 100, 2)), (box[0] + 200, box[1] + 30),
                                 cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
     if json_config is not None:
         cv2.rectangle(img,
                       (json_config["line_data"]["x_coord_f"], json_config["line_data"]["y_coord_f"]),
                       (json_config["line_data"]["x_coord_l"], json_config["line_data"]["y_coord_l"]),
-                      (255, 127, 0), 5)
+                      (0, 0, 255), 5)
 
-    return img, objectInfo
+    return img, object_info
